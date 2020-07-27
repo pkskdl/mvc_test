c541fc5de97fd422295b56cbf39c118096910f7f
priyesh <pkskadakkal@gmail.com>
2020-07-27 08:33:44 +0530

    final project

---
+++ Employee_mvc_webapi/DAL/EmployeeRepository.cs
@@ -0,0 +1,95 @@
+﻿using Dapper;
+using Employee_mvc_webapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Employee_mvc_webapi.DAL
+{
+    public class EmployeeRepository:IEmployeeRepository
+    {
+        private readonly IDbConnection _db;
+        
+        public EmployeeRepository()
+        {
+            _db = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+        }
+        bool IEmployeeRepository.Add(Employee employee)
+        {
+            int rowsAffected = this._db.Execute(@"INSERT Employee([Name],[Address],[Gender],[Company],[Designation]) 
+
+values (@Name, @Address, @Gender,@Company,@Designation)",
+                        new { Name = employee.Name,
+                            Address= employee.Address,
+                            Gender = employee.Gender,
+                            Company = employee.Company,
+                            Designation = employee.Designation
+
+                     
+
+
+        });
+
+            if (rowsAffected > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        bool IEmployeeRepository.Delete(string id)
+        {
+           
+           
+            int rowsAffected = this._db.Execute(@"DELETE FROM [Employee] WHERE Empid = @Empid",
+                new { Empid = id });
+
+            if (rowsAffected > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        Employee IEmployeeRepository.GetEmployee(string id)
+        {
+            return _db.Query<Employee>("SELECT * FROM [Employee] WHERE Empid =@Empid", new { Empid = id }).SingleOrDefault();
+        }
+
+        List<Employee> IEmployeeRepository.GetEmployees()
+        {
+            return _db.Query<Employee>("SELECT *  FROM [Employee]  ").ToList();
+        }
+
+        bool IEmployeeRepository.Update(Employee employee)
+        {
+            int rowsAffected = this._db.Execute(
+   "UPDATE [Employee] SET [Name] = @Name ,[Address] = @Address, [Gender] = @Gender,[Company]=@Company,Designation=@Designation WHERE Empid = " +
+                        employee.Empid, employee);
+
+            if (rowsAffected > 0)
+            {
+                return true;
+            }
+
+            return false;
+
+
+  //          int rowsAffected = this._db.Execute(
+  //"UPDATE [Employee] SET [Name] = @Name ,[Address] = @Address,[Gender] = @Gender,[Company]=@Company,[Designation]=@Designation WHERE Empid = " + employee.Empid,employee);
+
+  //          if (rowsAffected > 0)
+  //          {
+  //              return true;
+  //          }
+
+  //          return false;
+        }
+    }
+}
---
+++ Employee_mvc_webapi/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Employee_mvc_webapi.Models
+{
+    public class Employee
+    {
+        
+       
+        public string Empid { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Company { get; set; }
+        [Required]
+        public string Designation { get; set; }
+    }
+}
