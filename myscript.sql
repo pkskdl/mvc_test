df5aa6e466bdee7485abc7ada212f2c17054064c
pkskdl <pkskadakkal@gmail.com>
2020-08-03 11:50:36 +0530

    added new columns
    added new columns

--- myscript.sql
+++ myscript.sql
@@ -10,12 +10,14 @@ USE myapp
 go
 
 CREATE TABLE [dbo].[Employee] (
-    [EmpId]       INT          NOT NULL IDENTITY,
-    [Name]        VARCHAR (75) NULL,
-    [Address]     VARCHAR (50) NULL,
-    [Gender]      VARCHAR (10) NULL,
-    [Company]     VARCHAR (50) NULL,
-    [Designation] VARCHAR (50) NULL,
+    [EmpId]       INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]   VARCHAR (75)  NULL,
+    [LastName]    VARCHAR (30)  NULL,
+    [Emailid]     VARCHAR (50)  NULL,
+    [Address]     VARCHAR (100) NULL,
+    [Gender]      VARCHAR (10)  NULL,
+    [Company]     VARCHAR (50)  NULL,
+    [Designation] VARCHAR (50)  NULL,
     PRIMARY KEY CLUSTERED ([EmpId] ASC)
 );
 
