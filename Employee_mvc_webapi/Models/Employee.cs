caa008c99268113a264b68d05690e9c54568797a
pkskdl <pkskadakkal@gmail.com>
2020-08-03 11:36:03 +0530

    added firstname,lastname,email

--- Employee_mvc_webapi/Models/Employee.cs
+++ Employee_mvc_webapi/Models/Employee.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Employee_mvc_webapi.Models
 {
@@ -12,7 +13,14 @@ namespace Employee_mvc_webapi.Models
        
         public string Empid { get; set; }
         [Required]
-        public string Name { get; set; }
+       
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        public string FirstName { get; set; }
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string Emailid { get; set; }
         [Required]
         public string Address { get; set; }
         [Required]
